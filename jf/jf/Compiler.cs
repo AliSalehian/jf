3f0ad76282bf8a169efa2b712d1c9525a2220657
AliSalehian <ali.salehian77@gmail.com>
2021-10-20 18:32:02 +0330

    create simple tokenizer

---
+++ jf/jf/Compiler.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace jf
+{
+    class Compiler
+    {
+
+        private List<String> tokens;
+
+        public Compiler()
+        {
+            this.tokens = new List<String>();
+            this.tokenizer("test.txt");
+        }
+
+        public void tokenizer(string fileName)
+        {
+            string rawCode = File.ReadAllText("../../" + fileName);
+            string[] lines = rawCode.Split(' ');
+            foreach (var line in lines)
+            {
+                this.tokens.Add(line.Replace("\n", String.Empty));
+            }
+            foreach (var token in this.tokens)
+            {
+                Console.WriteLine(token);
+            }
+            Console.ReadLine();
+
+            /*Console.WriteLine(rawCode);
+            Console.ReadLine();*/
+        }
+
+
+        static void Main(string[] args)
+        {
+            Compiler c = new Compiler();
+        }
+    }
+}
