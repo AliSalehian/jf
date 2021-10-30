055b8c5445ae68d705ba556495c8eca61dcd717d
AliSalehian <ali.salehian77@gmail.com>
2021-10-30 18:51:09 +0330

    tokenizer and lineizer completed, creating symbol table started

--- jf/jf/Compiler.cs
+++ jf/jf/Compiler.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 using System.IO;
 
 namespace jf
@@ -23,17 +22,31 @@ namespace jf
 
         public List<Token> tokens;
         public List<String> lines;
+        public Explanation e = new Explanation();
 
         public Compiler()
         {
             this.tokens = new List<Token>();
             this.lines = new List<String>();
-            this.tokenizer("example.txt");
+            this.lineizer("example.txt");
+            this.fillExplanation();
         }
 
-        public void tokenizer(string fileName)
+        public string[] tokenizer(string line)
         {
-            int lineNumber = 1;
+            string[] result = new string[2];
+            string[] words = line.Split(' ');
+            result[0] = words[0];
+            for(int i=1; i < words.Length; i++)
+            {
+                result[1] += words[i] + " ";
+            }
+            return result;
+        } 
+
+        public void lineizer(string fileName)
+        {
+            int lineNumber = 0;
             using (StreamReader sr = new StreamReader("../../" + fileName))
             {
                 while(sr.Peek() >= 0)
@@ -44,20 +57,117 @@ namespace jf
                         continue;
                     }
                     string[] words = line.Split(' ');
-                    int wordNumber = 1;
+                    int wordNumber = 0;
                     foreach(var word in words)
                     {
                         if(word.Trim().Length == 0)
                         {
                             continue;
                         }
+                        if (word.Contains("<="))
+                        {
+                            string temp1 = word;
+                            temp1 = word.Replace("<=", " ");
+                            string[] temp2 = temp1.Split(' ');
+                            Token t1 = new Token(temp2[0].Trim(), lineNumber, wordNumber);
+                            wordNumber++;
+                            Token t2 = new Token("<=", lineNumber, wordNumber);
+                            wordNumber++;
+                            Token t3 = new Token(temp2[1], lineNumber, wordNumber);
+                            wordNumber++;
+                            this.tokens.Add(t1);
+                            this.tokens.Add(t2);
+                            this.tokens.Add(t3);
+                            continue;
+                        }
+
+                        if (word.Contains(">="))
+                        {
+                            string temp1 = word;
+                            temp1 = word.Replace(">=", " ");
+                            string[] temp2 = temp1.Split(' ');
+                            Token t1 = new Token(temp2[0].Trim(), lineNumber, wordNumber);
+                            wordNumber++;
+                            Token t2 = new Token(">=", lineNumber, wordNumber);
+                            wordNumber++;
+                            Token t3 = new Token(temp2[1], lineNumber, wordNumber);
+                            wordNumber++;
+                            this.tokens.Add(t1);
+                            this.tokens.Add(t2);
+                            this.tokens.Add(t3);
+                            continue;
+                        }
+
+                        if (word.Contains("="))
+                        {
+                            string temp1 = word;
+                            temp1 = word.Replace("=", " ");
+                            string[] temp2 = temp1.Split(' ');
+                            Token t1 = new Token(temp2[0].Trim(), lineNumber, wordNumber);
+                            wordNumber++;
+                            Token t2 = new Token("=", lineNumber, wordNumber);
+                            wordNumber++;
+                            Token t3 = new Token(temp2[1], lineNumber, wordNumber);
+                            wordNumber++;
+                            this.tokens.Add(t1);
+                            this.tokens.Add(t2);
+                            this.tokens.Add(t3);
+                            continue;
+                        }
+
+                        if (word.Contains("<"))
+                        {
+                            string temp1 = word;
+                            temp1 = word.Replace("<", " ");
+                            string[] temp2 = temp1.Split(' ');
+                            Token t1 = new Token(temp2[0].Trim(), lineNumber, wordNumber);
+                            wordNumber++;
+                            Token t2 = new Token("<", lineNumber, wordNumber);
+                            wordNumber++;
+                            Token t3 = new Token(temp2[1], lineNumber, wordNumber);
+                            wordNumber++;
+                            this.tokens.Add(t1);
+                            this.tokens.Add(t2);
+                            this.tokens.Add(t3);
+                            continue;
+                        }
+
+                        if (word.Contains(">"))
+                        {
+                            string temp1 = word;
+                            temp1 = word.Replace(">", " ");
+                            string[] temp2 = temp1.Split(' ');
+                            Token t1 = new Token(temp2[0].Trim(), lineNumber, wordNumber);
+                            wordNumber++;
+                            Token t2 = new Token(">", lineNumber, wordNumber);
+                            wordNumber++;
+                            Token t3 = new Token(temp2[1], lineNumber, wordNumber);
+                            wordNumber++;
+                            this.tokens.Add(t1);
+                            this.tokens.Add(t2);
+                            this.tokens.Add(t3);
+                            continue;
+                        }
                         Token t = new Token(word.Trim(), lineNumber, wordNumber);
                         this.tokens.Add(t);
                         wordNumber++;
                     }
-                    this.lines.Add(line);
+                    this.lines.Add(line.Trim());
                     lineNumber++;
                 }
+            }
+        }
+
+        public void fillExplanation()
+        {
+            foreach(var line in this.lines)
+            {
+                if (line.ToLower().Equals("begin"))
+                {
+                    break;
+                }
+                string[] restlt = this.tokenizer(line);
+                this.e.st.insert(restlt[0], restlt[1]);
             }
         }
 
@@ -66,12 +176,21 @@ namespace jf
         {
             int counter = 0;
             Compiler c = new Compiler();
-            foreach(var token in c.tokens)
+            SymbolTable s = new SymbolTable();
+            /*foreach(var token in c.tokens)
             {
                 string output = String.Format("token number {0} is '{1}', it is in line {2} and in this line is {3}th word", counter, token.value, token.lineNumber, token.indexInLine);
                 Console.WriteLine(output);
                 counter++;
                 Console.WriteLine();
+            }
+            foreach(var line in c.lines)
+            {
+                Console.WriteLine(line);
+            }*/
+            foreach(var temp in c.e.st.table)
+            {
+                Console.WriteLine(String.Format("identifier name is {0} and its attribute is {1}", temp[0], temp[1]));
             }
             Console.ReadLine();
         }
