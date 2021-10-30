fed46da776e8539301fcdf5cde202d3f257690da
AliSalehian <ali.salehian77@gmail.com>
2021-10-30 12:24:14 +0330

    tokenizer improved

--- jf/jf/Compiler.cs
+++ jf/jf/Compiler.cs
@@ -7,37 +7,73 @@ namespace jf
 {
     class Compiler
     {
+        public class Token
+        {
+            public string value;
+            public int lineNumber;
+            public int indexInLine;
 
-        private List<String> tokens;
+            public Token(string value, int lineNumber, int indexInLine)
+            {
+                this.value = value;
+                this.lineNumber = lineNumber;
+                this.indexInLine = indexInLine;
+            }
+        }
+
+        public List<Token> tokens;
+        public List<String> lines;
 
         public Compiler()
         {
-            this.tokens = new List<String>();
-            this.tokenizer("test.txt");
+            this.tokens = new List<Token>();
+            this.lines = new List<String>();
+            this.tokenizer("example.txt");
         }
 
         public void tokenizer(string fileName)
         {
-            string rawCode = File.ReadAllText("../../" + fileName);
-            string[] lines = rawCode.Split(' ');
-            foreach (var line in lines)
+            int lineNumber = 1;
+            using (StreamReader sr = new StreamReader("../../" + fileName))
             {
-                this.tokens.Add(line.Replace("\n", String.Empty));
+                while(sr.Peek() >= 0)
+                {
+                    string line = sr.ReadLine();
+                    if(line.Length == 0)
+                    {
+                        continue;
+                    }
+                    string[] words = line.Split(' ');
+                    int wordNumber = 1;
+                    foreach(var word in words)
+                    {
+                        if(word.Trim().Length == 0)
+                        {
+                            continue;
+                        }
+                        Token t = new Token(word.Trim(), lineNumber, wordNumber);
+                        this.tokens.Add(t);
+                        wordNumber++;
+                    }
+                    this.lines.Add(line);
+                    lineNumber++;
+                }
             }
-            foreach (var token in this.tokens)
-            {
-                Console.WriteLine(token);
-            }
-            Console.ReadLine();
-
-            /*Console.WriteLine(rawCode);
-            Console.ReadLine();*/
         }
 
 
         static void Main(string[] args)
         {
+            int counter = 0;
             Compiler c = new Compiler();
+            foreach(var token in c.tokens)
+            {
+                string output = String.Format("token number {0} is '{1}', it is in line {2} and in this line is {3}th word", counter, token.value, token.lineNumber, token.indexInLine);
+                Console.WriteLine(output);
+                counter++;
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
