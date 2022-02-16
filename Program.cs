821c9425e6ac84635e7244bf96772a25de7c45b0
AliSalehian <ali.salehian77@gmail.com>
2022-02-16 20:46:42 +0330

    add document of Program.cs file

--- Program.cs
+++ Program.cs
@@ -6,16 +6,21 @@ using System.Threading;
 
 namespace WindowsFormsApp1
 {
+    /// <summary>
+    /// class <c>Program</c> is main class that control backend and UI part of program
+    /// </summary>
     class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+        #region Global Variables
         static jf.Compiler compiler = new jf.Compiler();
         static Queue<jf.Command> queue = new Queue<jf.Command>();
         static jf.SensorHandler sensorHandler = new jf.SensorHandler();
         static List<string> fileNames = new List<string>();
+        #endregion
+
         [STAThread]
+
+        #region Start Threads
         static void Main()
         {
 
@@ -26,35 +31,42 @@ namespace WindowsFormsApp1
             UIThread.SetApartmentState(ApartmentState.STA);
             UIThread.Start();
         }
+        #endregion
 
         [STAThread]
+
+        #region UI Part
         public static void runUI()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1(Program.compiler, queue, fileNames));
         }
+        #endregion
 
+        #region Backend Part
         public static void runBackeEnd()
         {
+            #region Test Sensors Value
             sensorHandler.setSensor("T1", 80);
             sensorHandler.setSensor("T2", 130);
             sensorHandler.setSensor("T3", 80);
             sensorHandler.setSensor("n", 80);
+            #endregion
+
             while (true)
             {
                 if(Program.fileNames.Count != 0)
                 {
                     compiler.compile(Program.fileNames[0], queue);
                     jf.Runner runner = new jf.Runner(Program.compiler, queue, sensorHandler);
-                    int status = runner.run();
-                    Console.WriteLine(status);
+                    runner.run();
                     Program.fileNames.RemoveAt(0);
                     jf.Command clearTextBox = new jf.Command("delete ritchbox");
                     Program.queue.Enqueue(clearTextBox);
                 }
             }
         }
-
+        #endregion
     }
 }
